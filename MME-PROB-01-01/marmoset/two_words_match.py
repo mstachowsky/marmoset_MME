22846a9420c2d26ff0ec8728a34bfe1e05fef504
David Lau <d24lau@uwaterloo.ca>
2017-01-26 11:38:47 -0500

    add second test case

---
+++ MME-PROB-01-01/marmoset/two_words_match.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from subprocess import Popen,PIPE
+import re
+import sys
+
+input = ["./hello_world"]
+cproc=Popen(input, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+out,err=cproc.communicate()
+
+match_hello = re.search("hello", out, re.IGNORECASE)
+match_world = re.search("world", out, re.IGNORECASE)
+
+if match_hello and match_world :
+    print "passed - has the words 'hello' and 'world'"
+    sys.exit(0)
+elif not match_hello :
+    print "failed - missing the word 'hello'"
+    sys.exit(1)
+else :
+    print "failed - missing the word 'world'"
+    sys.exit(1)
