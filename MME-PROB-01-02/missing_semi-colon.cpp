fea4587a13976717fe1fcddb64d9c37a77e2eff2
David Lau <d24lau@uwaterloo.ca>
2017-02-01 13:55:33 -0500

    d24lau

--- MME-PROB-01-02/missing_semi-colon.cpp
+++ MME-PROB-01-02/missing_semi-colon.cpp
@@ -8,15 +8,15 @@
 using namespace std;
 
 int main()
-{	
+{
 	srand (time(NULL));
 
 	char play = 'x';
 	char play_again = 'y';
 	char difficulty = '1';
-	
+
 	cout << "Let's play 'Rock-Paper-Scissors!" << endl;
-	
+
 	while (play_again == 'y')
 	{
 		// 1: user always wins (because it looks at the user's play first)
@@ -32,25 +32,25 @@ int main()
 		do
 		{
 			cout << "Enter 1-4: ";
-			cin >> difficulty;			
+			cin >> difficulty;
 		}
 		while (difficulty < '1' || difficulty > '4');
-		
+
 		cout << endl;
-		
+
 		int play_num = 1;
 		char last_play = play;
 		char computer_play = 'r';
 		int player_score = 0;
 		int computer_score = 0;
-		
+
 		while (play_num <= 5)
 		{
 			cout << endl << "Round #" << play_num << " of 5" << endl;
 			do
 			{
 				cout << "Enter your play (r/p/s)";
-				cin >> play;				
+				cin >> play;
 			}
 			while (play != 'r' && play != 'p' && play != 's');
 
@@ -87,7 +87,7 @@ int main()
 				else
 					computer_play = 'r';
 			}
-		
+
 			if (play == 'r')
 				cout << "Your play:     rock";
 			else if (play == 'p')
@@ -100,7 +100,7 @@ int main()
 			if (computer_play == 'r')
 				cout << "rock" << endl;
 			else if (computer_play == 'p')
-				cout << "paper" << endl;
+				cout << "paper" << endl
 			else
 				cout << "scissors" << endl;
 
@@ -118,13 +118,13 @@ int main()
 				cout << "The computer wins this round." << endl;
 				computer_score++;
 			}
-			
+
 			cout << "Your score: " << player_score << "                ";
 			cout << "Computer score: " << computer_score << endl;
 
 			play_num++;
 		}
-		
+
 		cout << endl;
 		if (player_score > computer_score)
 			cout << "You win this game!" << endl;
@@ -132,18 +132,18 @@ int main()
 			cout << "The computer wins this game!" << endl;
 		else
 			cout << "The game is a tie!" << endl;
-		
+
 		cout << "Would you like to play again?" << endl;
 		do
 		{
 			cout << "Enter y/n: ";
-			cin >> play_again;			
+			cin >> play_again;
 		}
 		while (play_again != 'y' && play_again != 'n');
-		
+
 		cout << endl << endl << endl;
 	}
 
-	
+
 	return 0;
 }
