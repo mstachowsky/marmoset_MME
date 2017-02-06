b145cb79d90d73143a6df5074588679799d412a8
David Lau <d24lau@uwaterloo.ca>
2017-02-05 20:28:42 -0500

    d24lau

--- MME-PROB-02-01/Problem Description.md
+++ MME-PROB-02-01/Problem Description.md
@@ -4,17 +4,29 @@ Problem Number: MME-PROB-02-01
 Problem Title: Steel MSDS
 =========================
 
+Code filename: steel_MSDS.cpp
+
 Write a program that provides a form for the user to fill out some Material Safety Data Sheet information. The form must have exactly the same format as the following example:
 
-SDS ID No.: 9994
-Product Identifier: Cold Rolled Sheet Steel
-Carcinogenity: 2
-Reproductive Toxicity: 2
-STOT Repeat Exposure: 1
-Composition (% weight):
-Iron: 95
-Manganese: 2.225
-Chromium: 0.65
-Nickel: 0.25
+    SDS ID No.: 9994
+    Product Identifier: Cold Rolled Sheet Steel
+    Carcinogenity: 2
+    Reproductive Toxicity: 2
+    STOT Repeat Exposure: 1
+    Composition (% weight):
+    Iron: 95
+    Manganese: 2.225
+    Chromium: 0.65
+    Nickel: 0.25
+
+Note that the Product Identifier will always be composed of four words.
+
+After the user has entered the information, reproduce the information in a compressed format:
+
+    SDS: 9994     Identifier: Cold Rolled Sheet Steel
+    Hazard: 2-2-1
+    Composition:
+    Fe    Mn    Cr    Ni
+    95    2.225 0.65  0.25
 
 You may reference the three included MSDS documents to see real-world examples of these types of documents.
---
+++ MME-PROB-02-01/marmoset/canonical/steel_MSDS.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int SDS = 0;
+  string product_identifier[4];
+  int carcinogenity = 0;
+  int reproductive_toxicity = 0;
+  int STOT_repeat_exposure = 0;
+  double composition_iron = 0.0;
+  double composition_manganese = 0.0;
+  double composition_chromium = 0.0;
+  double composition_nickel = 0.0;
+
+  cout << "SDS ID No.: "
+  cin >> SDS;
+
+  cout << "Product Identifier: "
+  for (int word_num = 0; word_num < 4; word_num++)
+    cin >> product_identifier[word_num];
+
+  cout << "Carcinogenity: "
+  cin >> carcinogenity;
+
+  cout << "Reproductive Toxicity: "
+  cin >> reproductive_toxicity;
+
+  cout << "STOT Repeat Exposure: "
+  cin >> STOT_repeat_exposure;
+
+  cout << "Composition (% weight):" << endl;
+  cout << "Iron: "
+  cin >> composition_iron;
+
+  cout << "Manganese: "
+  cin >> composition_manganese;
+
+  cout << "Chromium: "
+  cin >> composition_chromium;
+
+  cout << "Nickel: "
+  cin >> composition_nickel;
+
+  cout << "SDS: " << SDS << "     Identifier: ";
+  for (int word_num = 0; word_num < 4; word_num++)
+    cout << product_identifier[word_num];
+
+  cout << "Hazard: "
+       << carcinogenity << "-"
+       << reproductive_toxicity << "-"
+       << STOT_repeat_exposure << endl;
+
+  cout << "Composition:" << endl;
+  cout << "Fe    Mn    Cr    Ni" << endl;
+  cout << composition_iron << "    "
+       << composition_manganese << " "
+       << composition_chromium << "  "
+       << composition_nickel << endl;
+
+  return 0;
+}
